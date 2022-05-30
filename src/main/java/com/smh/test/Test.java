d92b17edd00b73e5bf912df1e3f496ee97b97dfd
smh <smh@qq.com>
2022-05-30 11:06:03 +0800

    333

--- src/main/java/com/smh/test/Test.java
+++ src/main/java/com/smh/test/Test.java
@@ -9,5 +9,6 @@ package com.smh.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("test111111111");
+        System.out.println("test222222222222");
     }
 }
