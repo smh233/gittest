726095672b9d0e7a68ec60c393d155aa4c586a04
smh <smh@qq.com>
2022-05-30 11:16:54 +0800

    master commit2

--- src/main/java/com/smh/test/Test.java
+++ src/main/java/com/smh/test/Test.java
@@ -11,5 +11,6 @@ public class Test {
         System.out.println("test111111111");
         System.out.println("test222222222222");
         System.out.println("333333333333333");
+        System.out.println("master test");
     }
 }
