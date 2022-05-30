130ee42f442feda54c98cc848273fbf4d04a0e3c
smh <smh@qq.com>
2022-05-30 11:12:29 +0800

    hot-fix commit

--- src/main/java/com/smh/test/Test.java
+++ src/main/java/com/smh/test/Test.java
@@ -10,5 +10,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("test111111111");
         System.out.println("test222222222222");
+        System.out.println("333333333333333");
     }
 }
