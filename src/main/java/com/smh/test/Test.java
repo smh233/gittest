9ae719cb8d4bd673b1c0b25b4552116ed861c888
smh <smh@qq.com>
2022-05-30 11:05:36 +0800

    222

---
+++ src/main/java/com/smh/test/Test.java
@@ -0,0 +1,13 @@
+package com.smh.test;
+
+/**
+ * @author: smh
+ * @create: 2022/5/30
+ * @Description:
+ * @FileName: Test
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test111111111");
+    }
+}
